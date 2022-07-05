1a2c9d0a89efdacfb471c0d66d8b06ea0cb48aaa
corvotorvo <83085402+MAlcont@users.noreply.github.com>
2022-07-05 09:27:47 +0200

    Completed module

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,15 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct,
+getProducts,
+getProductWithID } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
+    .get(getProducts)
 
     .post(addnewProduct);
+
+    app.route('/products/:ProductID')
+    .get(getProductWithID)
 }
 
 export default routes;
