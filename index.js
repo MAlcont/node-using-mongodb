1866616c09fc510e821b4ad75cb66abe3faecc3d
corvotorvo <83085402+MAlcont@users.noreply.github.com>
2022-07-05 09:08:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import routes from './src/routes/routes';
+
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
@@ -14,6 +16,9 @@ mongoose.connect('mongodb://localhost/productsdb', {
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+routes(app);
+
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
